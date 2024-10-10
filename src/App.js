2f4d543c2cf32f202394a90e8e14ad5a16cd6aa2
ahmadsohailMask01 <ahmaddd.sohail759@gmail.com>
2024-10-10 13:49:48 +0500

    Voting App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // App.js
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { getFirestore, updateDoc, doc, increment } from "firebase/firestore";
 import { app } from "./Firebase";
-import './App.css';
+import "./App.css";
 // import fg from './fg.jpg';
-import cal from './Calvin.png';
-import ani from './Anishka.png';
-import ary from './Aryan.jpeg';
-import kri from './Kritika.png';
-import moh from './Mohanty.png';
-import pun from './Punit.png';
-import raj from './Rajani.png';
-import rish from './Rishi.png';
-import Card from './Card';
-import { ToastContainer } from 'react-toastify';
+import cal from "./Calvin.jfif";
+import ani from "./Anishka.jfif";
+import ary from "./Aryan.png";
+import kri from "./Kritika.png";
+import moh from "./Mohanty.jfif";
+import pun from "./Punit.jfif";
+import raj from "./Rajani.jfif";
+import rish from "./Rishi.jfif";
+import Card from "./Card";
+import { ToastContainer } from "react-toastify";
 
 // Initialize Firestore
 const db = getFirestore(app);
@@ -66,8 +66,8 @@ export default class App extends Component {
     const { count, reset } = this.state;
 
     return (
-      <div className='App'>
-       <ToastContainer
+      <div className="App">
+        <ToastContainer
           position="top-right"
           autoClose={3000}
           hideProgressBar={false}
@@ -79,66 +79,64 @@ export default class App extends Component {
           pauseOnHover
           theme="colored"
         />
-        <p className='header'>✨VOTING SYSTEM✨</p>
-        
-          <div className='row'>
-            <Card
-              count={count}
-              reset={reset}
-              onButtonClick={this.handleCardButtonClick}
-              name1="A S Calvin"
-              name2="Singh Rishiraj"
-              category="Head-Boy(Assistant)"
-              onVote={this.updateCandidateVotes}
-              image1={cal}
-              image2={rish}
-            />
-            <Card
-              count={count}
-              reset={reset}
-              onButtonClick={this.handleCardButtonClick}
-              name1="Sharma Rajani"
-              name2="Mishra Anshika"
-              category="Head-Girl(Assistant)"
-              onVote={this.updateCandidateVotes} // Add this line
-              image1={raj}
-              image2={ani}
-             
-            />
-            <Card
-              count={count}
-              reset={reset}
-              onButtonClick={this.handleCardButtonClick}
-              name1="Vaishnav Punit"
-              name2="Patel Aryan"
-              category="Sports-Captain(Boy)"
-              onVote={this.updateCandidateVotes}
-              image1={pun}
-              image2={ary}
-              
-              
-              
-              />
+        <p
+          className="header"
+          style={{
+            background: `linear-gradient(to right, #0c1337, #0c1674, #0a85d9)`,
+            cursor: `pointer`,
+          }}
+        >
+          ✨E-entikhab VOTING SYSTEM✨
+        </p>
 
+        <div className="row" style={{ margin: `1%`, marginTop: `2%` }}>
+          <Card
+            count={count}
+            reset={reset}
+            onButtonClick={this.handleCardButtonClick}
+            name1="A S Calvin"
+            name2="Singh Rishiraj"
+            category="Head-Boy(Assistant)"
+            onVote={this.updateCandidateVotes}
+            image1={cal}
+            image2={rish}
+            style={{ width: `50%` }}
+          />
+          <Card
+            count={count}
+            reset={reset}
+            onButtonClick={this.handleCardButtonClick}
+            name1="Sharma Rajani"
+            name2="Mishra Anshika"
+            category="Head-Girl(Assistant)"
+            onVote={this.updateCandidateVotes} // Add this line
+            image1={raj}
+            image2={ani}
+          />
+          <Card
+            count={count}
+            reset={reset}
+            onButtonClick={this.handleCardButtonClick}
+            name1="Vaishnav Punit"
+            name2="Patel Aryan"
+            category="Sports-Captain(Boy)"
+            onVote={this.updateCandidateVotes}
+            image1={pun}
+            image2={ary}
+          />
 
-              <Card
-              count={count}
-              reset={reset}
-              onButtonClick={this.handleCardButtonClick}
-              name1="Dubey Kritika"
-              name2="Mohanty Jyoti"
-              category="Sports-Captain(Girl)"
-              onVote={this.updateCandidateVotes}
-              image1={kri}
-              image2={moh}
-              
-              
-              
-              />
-
-              
-          </div>
-        
+          <Card
+            count={count}
+            reset={reset}
+            onButtonClick={this.handleCardButtonClick}
+            name1="Dubey Kritika"
+            name2="Mohanty Jyoti"
+            category="Sports-Captain(Girl)"
+            onVote={this.updateCandidateVotes}
+            image1={kri}
+            image2={moh}
+          />
+        </div>
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,9 @@
-import React, { PureComponent } from 'react';
-import './Card.css';
+import React, { PureComponent } from "react";
+import "./Card.css";
 // import fg from './fg.jpg'; // Ensure this path is correct for your image
-import Button from './Button';
-import {  toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Button from "./Button";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default class Card extends PureComponent {
   state = {
@@ -18,21 +18,20 @@ export default class Card extends PureComponent {
   }
 
   notify = (name) => {
-    toast.success(`You voted for ${name}`,{
+    toast.success(`You voted for ${name}`, {
       position: "top-right",
-autoClose: 3000,
-hideProgressBar: false,
-closeOnClick: true,
-pauseOnHover: true,
-draggable: true,
-progress: undefined,
-theme: "colored",
-
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
     });
   };
 
   // Handle button click events
-  handleButtonClick = (category, candidate,name) => {
+  handleButtonClick = (category, candidate, name) => {
     this.setState({ clicked: true });
     this.props.onButtonClick(); // Notify the parent component
     this.props.onVote(category, candidate);
@@ -44,34 +43,46 @@ theme: "colored",
     const { category, name1, name2 } = this.props;
 
     return (
-      <div className='card_first'>
-        
-        <p className='sport'>{category}</p>
-        <div className='card_second'>
+      <div
+        className="card_first"
+        style={{ background: `linear-gradient(50deg, #0c4321, #478d)` }}
+      >
+        <p className="sport" style={{ marginBottom: `2%` }}>
+          {category}
+        </p>
+        <div className="card_second">
           {clicked ? (
-            <div className='thank-you-animation'>
-              <div className='thank-you-text'>Thank You!😃</div>
-              <div className='thank-you-background'></div>
+            <div className="thank-you-animation">
+              <div className="thank-you-text">Thank You!😃</div>
+              <div className="thank-you-background"></div>
               {/* Sparkles for animation */}
-              <div className='sparkle'></div>
-              <div className='sparkle'></div>
-              <div className='sparkle'></div>
-              <div className='sparkle'></div>
-              <div className='sparkle'></div>
+              <div className="sparkle"></div>
+              <div className="sparkle"></div>
+              <div className="sparkle"></div>
+              <div className="sparkle"></div>
+              <div className="sparkle"></div>
             </div>
           ) : (
             <>
-              <div className='left'>
+              <div className="left">
                 <img src={this.props.image1} alt="Candidate" />
-                <div className='candidate-name'>{name1}</div>
-                <Button handleClick={() => this.handleButtonClick(category, 'Candidate 1',name1)} label="Vote for Candidate 1" />
-                
+                <div className="candidate-name">{name1}</div>
+                <Button
+                  handleClick={() =>
+                    this.handleButtonClick(category, "Candidate 1", name1)
+                  }
+                  label="Vote for Candidate 1"
+                />
               </div>
-              <div className='middle'>
+              <div className="middle">
                 <img src={this.props.image2} alt="Candidate" />
-                <div className='candidate-name'>{name2}</div>
-                <Button handleClick={() => this.handleButtonClick(category, 'Candidate 2',name2)} label="Vote for Candidate 2" />
-                
+                <div className="candidate-name">{name2}</div>
+                <Button
+                  handleClick={() =>
+                    this.handleButtonClick(category, "Candidate 2", name2)
+                  }
+                  label="Vote for Candidate 2"
+                />
               </div>
 
               {/* <div className='right'>
@@ -86,6 +97,5 @@ theme: "colored",
     );
   }
 }
-
 
 ///candidates/vote/Head-Boy/Candidate 1
